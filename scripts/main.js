eeb87f67ae76e9ed6764afb8f3d857e7402aae39
kyler <7578101@isd535.org>
2022-02-15 11:33:21 -0600

    ghkhgmhgf

--- scripts/main.js
+++ scripts/main.js
@@ -10,10 +10,35 @@ let shipHeight = 20
 let rightPressed = false;
 let leftPressed = false;
 
+let shootRadius = 3
+let dx = 5;
+let n = 0;
+let k = shipX;
+let spacebar = false;
+let ds = 5;
+const bullets = [];
+
+function test(){
+    ctx.beginPath();
+    ctx.arc(100,100,20,0,2*Math.PI);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
 
 
-
+function drawShoot() {
+    bullets.push(shipX + shipWidth/2);
+    bullets.push(canvas.height-shipHeight-shootRadius);
+    ctx.beginPath();
+    ctx.arc(k,n,shootRadius,0,2*Math.PI)
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    n-=ds;
+    
+}
 
 function drawShip(){//draws the ship
     ctx.beginPath();
@@ -26,22 +51,30 @@ function drawShip(){//draws the ship
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawShip();
-}
-
-
-
-if(rightPressed) {
-    shipX += 0;
-    if (shipX + shipWidth > canvas.width){
-        shipX = canvas.width - shipWidth;
+    if (spacebar){
+        k = shipX + shipWidth/2;
+        n = canvas.height-shipHeight-shootRadius;
     }
-}
-else if(leftPressed) {
-    shipX -= 0;
-    if (shipX < 0){
-        shipX = 0;
+    drawShoot();
+
+    if(rightPressed) {
+        shipX += 5;
+        if (shipX + shipWidth > canvas.width){
+            shipX = canvas.width - shipWidth;
+        }
     }
+    else if(leftPressed) {
+        shipX -= 5;
+        if (shipX < 0){
+            shipX = 0;
+        }
+    }
+
+    //test();
+
 }
+
+
 
 function keyDownHandler(e) {
     if(e.key=="Right" || e.key == "ArrowRight") {
@@ -50,14 +83,20 @@ function keyDownHandler(e) {
     else if(e.key == "left" || e.key == "ArrowLeft") {
         leftPressed = true;
     }
+    if(e.key=="ArrowUp" || e.code == "Space") {
+        spacebar = true;
+    }
 }
 
 function keyUpHandler(e) {
     if(e.key=="Right" || e.key == "ArrowRight") {
         rightPressed = false;
     }
-    else if(e.key == "left" || e.key == "ArrowLeft") {
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
+    }
+    if(e.key=="ArrowUp" || e.code == "Space") {
+        spacebar = false;
     }
 }
 
